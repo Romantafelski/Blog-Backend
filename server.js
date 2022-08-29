deb9938dd53e360063eb84f835b222dac6a071cb
sdam abdi <sdamabdi@sdams-MacBook-Pro.local>
2022-08-29 10:45:08 -0500

    test

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session')
 const Blog = require('./models/blog.js')
+const User = require('./models/users.js')
 const userController = require('./controllers/users_controller.js')
 const sessionsController = require('./controllers/sessions_controller.js')
 require('dotenv').config()
